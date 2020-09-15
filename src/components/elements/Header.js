242297ee70758e5961b60d813ded0b5c1a2a0de5
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-14 17:23:28 -0700

    Start Styled Components 3 - Global Styling

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -14,46 +14,6 @@ import {
 //2. Handling props in styled component
 //3. Creating a global stylewith styled components
 
-// const StyledHeader = styled.div`
-//   background: #1c1c1c;
-//   padding: 0 20px;
-//   box-sizing: border-box;
-
-//   .header-content {
-//     max-width: 1280px;
-//     min-height: 120px;
-//     padding: 20px 0;
-//     margin: 0 auto;
-//     box-sizing: border-box;
-
-//     @media screen and (max-width: 500px){
-//       min-height: 0px;
-//     }
-//   }
-// `;
-
-// const StyledRMDBLogo = styled.img`
-//   width: 250px;
-//   margin-top: 20px;
-
-//   @media screen and (max-width: 500px){
-//     width: 150px;
-//     margin-top: 5px;
-//   }
-// `;
-
-// const StyledTMDBLogo = styled.img`
-//   width: 122px;
-//   margin-top: 25px;
-//   float: right;
-
-//   @media screen and (max-width: 500px){
-//     display: inline-block;
-//     width: 80px;
-//     margin-top: 0px;
-//   }
-// `;
-
 
 const Header = () => (
   <StyledHeader>
