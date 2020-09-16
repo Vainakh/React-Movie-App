35a45b7932077a1e975509634dcbcbfaa0c73eaa
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-15 18:47:40 -0700

    Start SearchBar - Controlled components

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+import { StyledSearchBar, StyledSearchBarContent} from '../styles/StyledSearchBar';
 
 const SearchBar = () => <div>SearchBar</div>
 
