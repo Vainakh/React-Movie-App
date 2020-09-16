3d70f03eda873fefef1198274aed5b2228bbb8f9
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-16 08:50:33 -0700

    Routing with Reach Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './elements/Header';
 import Home from './Home';
+import { Router } from '@reach/router';
 
 import { GlobalStyle } from './styles/GlobalStyle';
 
